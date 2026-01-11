0dc2d777448afa4b52d34927c0d6bf04f3ab78f8
Zaki Ayoubi <zakihorakhsh2028@gmail.com>
2026-01-11 15:58:13 -0800

    yy

--- server/app/main.py
+++ server/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import logging
+import os
 
 app = FastAPI()
 
+origins = os.getenv("ALLOWED_ORIGINS")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
